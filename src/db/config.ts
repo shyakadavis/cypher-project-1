c264604031ac4cf098c31c46aec541e5d0ffd374
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-01-26 09:43:49 +0200

    chore: update routing mechanism

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,4 +1,8 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+// configure dotenv. This is done once, don't re-configure it anywhere else.
+dotenv.config();
 
 const dbName = process.env.DB_NAME as string;
 const dbUser = process.env.DB_USER as string;
@@ -6,12 +10,14 @@ const dbHost = process.env.DB_HOST;
 const dbPassword = process.env.DB_PASSWORD;
 // dialect needs to explicity stated - that is why it was changed from the previous commits where the driver was accessed from .env
 // if you try to define the driver from the .env it will throw a fat error ❗👺
+
+const sequelize = new Sequelize(`${dbName}`, `${dbUser}`, `${dbPassword}`, {
+  host: dbHost,
+  dialect: 'postgres',
+});
+
 const sequelizeConnection = async () => {
   try {
-    const sequelize = new Sequelize(`${dbName}`, `${dbUser}`, `${dbPassword}`, {
-      host: dbHost,
-      dialect: 'postgres',
-    });
     await sequelize.authenticate();
     console.log(` 🟢 Successfully connected to the db... 😄 🟢`);
   } catch (error) {
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,27 +1,20 @@
-import User, { UserMap } from '../db/models/user.model';
-import sequelizeConnection from '../db/config';
 import { Router, Request, Response } from 'express';
 
-const router = Router();
+const userRouter = Router();
 
 // GET - users
-router.get('/users', async (req: Request, res: Response) => {
-  UserMap(sequelizeConnection);
-  const result = await User.findAll();
-  res.status(200).json({ users: result });
-}); // GET - users/:id
-router.get('/user/:id', async (req: Request, res: Response) => {
-  UserMap(sequelizeConnection);
-  const id = Number(req.params.id);
-  const result = await User.findByPk(id);
-  res.status(200).json({ user: result });
-}); // POST - users
-router.post('/user', async (req: Request, res: Response) => {
-  let newUser = req.body as User;
-  UserMap(sequelizeConnection);
-  const result = await User.create(newUser);
-  newUser = result.dataValues as User;
-  res.status(201).json({ user: newUser });
+userRouter.get('/all', (req: Request, res: Response) =>
+  res.status(404).json({
+    message: 'Here',
+  }),
+);
+// GET - users/:id
+userRouter.get('/:id', async (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Working' });
+});
+// POST - users
+userRouter.post('/', async (req: Request, res: Response) => {
+  res.status(201).json({ message: 'Working' });
 });
 
-export default router;
+export default userRouter;
