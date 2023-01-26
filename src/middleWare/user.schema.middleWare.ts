58dd676a9b37302d0f3da3d8ee9e8382b362e2eb
ISHIMWE-Aime <fabricei645@gmail.com>
2023-01-26 19:21:20 +0200

    ft: add validation middleware

--- src/middleWare/user.schema.middleWare.ts
+++ src/middleWare/user.schema.middleWare.ts
@@ -1,26 +1,39 @@
-import exp from "constants";
-import Joi from "joi";
-import { User } from "../db/schemas";
-export const validateLoginData = (login: {email: string, password: string}) => {
-    const loginSchema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().regex(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/).required()
-    })
+import Joi from 'joi';
+import { User } from '../db/schemas';
+import { IUser } from '../interfaces';
+export const validateLoginData = (login: {
+  email: string;
+  password: string;
+}) => {
+  const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(
+        /^(((?=.*[a-z])(?=.*[A-Z]))((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/,
+      )
+      .required(),
+  });
 
-    return loginSchema.validate(login)
-}
+  return loginSchema.validate(login);
+};
 
-export const validateUserData = (user: { email: string, password: string, googleId: string, province: string, district: string, sector: string, cell: string, street: string}) => {
-    const userInfoSchema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().regex(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/).required(),
-        googleId: Joi.string().required(),
-        province: Joi.string().required(),
-        district: Joi.string().required(),
-        sector: Joi.string().required(),
-        cell: Joi.string().required(),
-        street: Joi.string().required(),
-    })
+export const validateUserData = (user: IUser) => {
+  const userInfoSchema = Joi.object<IUser>({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(
+        /^(((?=.*[a-z])(?=.*[A-Z]))((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/,
+      )
+      .required(),
+    googleId: Joi.string(),
+    province: Joi.string().required(),
+    district: Joi.string().required(),
+    sector: Joi.string().required(),
+    cell: Joi.string().required(),
+    street: Joi.string().required(),
+    surName: Joi.string().required(),
+    givenName: Joi.string().required(),
+  });
 
-    return userInfoSchema.validate(user)
-}
+  return userInfoSchema.validate(user);
+};
