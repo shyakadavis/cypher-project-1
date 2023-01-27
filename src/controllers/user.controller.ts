f2135094a80a6ddf0409a5e786cbea73a50e5dab
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-01-27 14:53:56 +0200

    chore: add update address & get one user functions (#10)

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
-import { createUserService, findAllUsersService } from '../services';
+import {
+  createUserService,
+  findAllUsersService,
+  findOneUserService,
+  updateUserInfoService,
+} from '../services';
 import { User } from '../db/schemas';
 
 // fetch all user from the db
@@ -29,16 +34,14 @@ export const createUser = async (req: Request, res: Response) => {
       where: { email: newUser.email },
     });
     if (thisUserExists) {
-      return res
-        .status(400)
-        .json({
-          status: 400,
-          success: false,
-          message: `This email is already in use 🤡`,
-        });
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: `This email is already in use 🤡`,
+      });
     } else {
       const createdUser = await createUserService(newUser);
-      res.status(200).json({ status: 200, success: true, data: [createdUser] });
+      res.status(201).json({ status: 201, success: true, data: [createdUser] });
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -53,3 +56,54 @@ export const createUser = async (req: Request, res: Response) => {
 };
 
 // fetch one user from the db
+export const getOneUser = async (req: Request, res: Response) => {
+  try {
+    const wantedUser = req.params.id;
+    const oneUser = await findOneUserService(wantedUser);
+    if (oneUser) {
+      res.status(200).json({ status: 200, success: true, data: oneUser });
+    } else {
+      res
+        .status(404)
+        .json({ status: 404, success: false, message: `User not found 😶` });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(` 🔴 Error fetching user: 😟 ${error.message} 🔴`);
+      res
+        .status(500)
+        .json({ status: 500, success: false, message: `${error.message}` });
+    } else {
+      console.log('Unexpected error', error);
+    }
+  }
+};
+
+// update billing address
+export const updateBillingAddress = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const updateData = req.body;
+    const theUser = await findOneUserService(userId);
+    if (theUser) {
+      const updatedAddress = await updateUserInfoService(userId, updateData);
+      res.status(201).json({ status: 201, success: true, data: updatedAddress });
+
+    } else {
+      res
+        .status(404)
+        .json({ status: 404, success: false, message: `User not found 😶` });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(
+        ` 🔴 Error updating the billing address: 😟 ${error.message} 🔴`,
+      );
+      res
+        .status(500)
+        .json({ status: 500, success: false, message: `${error.message}` });
+    } else {
+      console.log('Unexpected error', error);
+    }
+  }
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { User } from '../db/schemas';
-import { IUser } from '../interfaces';
+import { IUpdateData, IUser } from '../interfaces';
 
 export const findAllUsersService = async () => {
   const findUsersRequest = await User.findAll();
@@ -7,9 +7,30 @@ export const findAllUsersService = async () => {
 };
 
 // find one user
-
+export const findOneUserService = async (userId: string) => {
+  const findOneUserRequest = await User.findOne({ where: { id: userId } });
+  return findOneUserRequest;
+};
 // create user
 export const createUserService = async (newUser: IUser) => {
   const createUserRequest = await User.create(newUser);
   return createUserRequest;
+};
+
+// update user info - their billing address
+export const updateUserInfoService = async (
+  userId: string,
+  updateData: IUpdateData,
+) => {
+  const updateUserInfoRequest = await User.update(
+    {
+      province: updateData.province,
+      district: updateData.district,
+      sector: updateData.sector,
+      cell: updateData.cell,
+      street: updateData.street,
+    },
+    { where: { id: userId } },
+  );
+  return updateUserInfoRequest;
 };
